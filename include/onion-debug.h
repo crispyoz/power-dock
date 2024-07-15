3b80bde175ef4f42f472b70f2bd4779ed33cf81a
crispyoz <36563657+crispyoz@users.noreply.github.com>
2024-07-15 20:00:19 +1000

    Update onion-debug.h

--- include/onion-debug.h
+++ include/onion-debug.h
@@ -1,1 +1,21 @@
 
+onionPrint(ONION_SEVERITY_DEBUG, "   Pulsing ctrl pin: GPIO%d\n", ctrlGpio);
+
+#ifndef _ONIONDEBUG_H_
+#define _ONIONDEBUG_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void onionPrint(int verbosity, char *s, ...);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // _ONIONDEBUG_H_
